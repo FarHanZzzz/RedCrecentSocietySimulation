11eb0491a3f7180293605d4ec2e011fe24b177b5
FarHanZzzz <farhansad84@gmail.com>
2025-04-14 16:07:26 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module oopfinal.redcrecentsocietysimulation {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens oopfinal.redcrecentsocietysimulation to javafx.fxml;
+    exports oopfinal.redcrecentsocietysimulation;
+}
